50801d5021ac405fc741e906e7e08d40aeabc2a5
chelseamozroth@gmail.com <visual_comchelseamozroth@gmail.community@outlook.com>
2020-02-29 10:37:32 -0400

    suma y multiplicacion

---
+++ Webapicalculadora/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webapicalculadora.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        public CalculatorController()
+        {
+
+        }
+        [Route("Multiply")]
+        [HttpPost]
+        public int Multiply([FromHeader]int a, [FromHeader] int b, int c)
+        {
+            return a * b; 
+        }
+        [Route("Multiply")]
+        [HttpGet]
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
